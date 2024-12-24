aa7f72c5b150d929c52675254d9e25a2280b93e8
WanhedaSt <storewanheda@gmail.com>
2024-12-24 01:34:19 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,17 +1,3 @@
-changelogs = {
-    "1": {
-        "date": "2024-12-01",
-        "version": "6.2.10",
-        "description": "Corrigido bug no modo 'Aim Notepad'. Melhorias gerais de desempenho."
-    },
-    "2": {
-        "date": "2024-11-25",
-        "version": "6.2.9",
-        "description": "Adicionado suporte para novos monitores 144Hz. Melhorias na interface do usuário."
-    },
-    "3": {
-        "date": "2024-11-15",
-        "version": "6.2.8",
-        "description": "Atualização de compatibilidade com o Windows 11. Correções de erros menores."
-    }
-}
+[+] Loader Atualizado
+[+] Suporte HVCI Corrigido
+[+] Teste
